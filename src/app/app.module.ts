2e73c04a33385204f1be8a231c9a110f4bdc89b5
Derek Krebs <derekkrebs@comcast.net>
2019-10-30 21:26:01 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { NewTodoItemComponent } from './new-todo-item/new-todo-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    TodoItemComponent,
+    TodoListComponent,
+    NewTodoItemComponent
   ],
   imports: [
     BrowserModule,
